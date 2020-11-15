8f126e7db84edfd9ec509c016142f5ac6b2e4f89
mgarbiso <markus.a.garbiso@gmail.com>
2020-11-15 17:20:10 -0600

    added Transaction struct

--- src/Ledger.jl
+++ src/Ledger.jl
@@ -1,8 +1,22 @@
 module Ledger
 
+using Dates
+
+"""`LedgerModification` is an `abstract type` that encompasses all the modifications on can do to a ledger."""
+abstract type LedgerModification end
+
+struct Transcation<:LedgerModification
+    amount::Real
+    account::String
+    date::Union{Date,DateTime}
+    tags::Dict{String,String}
+end
+
 # Transactions
-# make_transaction (for expenses and incomes)
-# modify_accounts add account, delete account, default account
+# add_transaction! (for expenses and incomes)
+# remove_account! add account, delete account
+# add_account! add account, delete add_account
+# set_default_account! default account
 
 #Read read_net
 # Display Current Net
