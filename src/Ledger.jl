884e74e1fa160b47b2860f1b9084cf9bcc1f424c
mgarbiso <markus.a.garbiso@gmail.com>
2020-11-15 13:04:50 -0600

    first commit

---
+++ src/Ledger.jl
@@ -0,0 +1,22 @@
+module Ledger
+
+# Transactions
+# make_transaction (for expenses and incomes)
+# modify_accounts add account, delete account, default account
+
+#Read
+# Display Current Net
+# Display Curent Avaialable/Expected
+# Display Unallocated funds, funds with no category.
+# Display Filtered funds
+
+# Export
+# csv
+# pdf report
+# excel
+
+# Import
+# csv
+
+
+end # module
