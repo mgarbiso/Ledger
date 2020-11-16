db1cbbd2c90e0dc9eaff8f2d3cbd3b5eae795b17
mgarbiso <markus.a.garbiso@gmail.com>
2020-11-15 20:27:14 -0600

    added remove_accountadd_account! addtransaction! to working implementation

--- quick_code.jl
+++ quick_code.jl
@@ -6,7 +6,18 @@ using Dates, Ledger, Random
 
 ledger = new_ledger()
 
-for i in 1:10
-    add_transaction!(ledger, Transaction(rand(-10:10), randstring('a':'z',5), today(), Dict()))
+account_names = ["Food", "Transportation", "Fashion"]
+
+for account in account_names
+    add_account!(ledger, account)
 end
 
+push!(account_names, "Education")
+
+for i in 1:10
+    add_transaction!(ledger, Transaction(rand(-10:10), rand(account_names), today(), Dict()))
+end
+
+for account in account_names
+    remove_account!(ledger, account)
+end
--- src/Ledger.jl
+++ src/Ledger.jl
@@ -12,14 +12,62 @@ struct Transaction<:LedgerEntry
     tags::Dict{String,String}
 end
 
+struct AddAccount<:LedgerEntry
+    account::String
+    date::Union{Date,DateTime}
+end
+
+struct RemoveAccount<:LedgerEntry
+    account::String
+    date::Union{Date,DateTime}
+end
 
 function new_ledger()
     LedgerEntry[]
 end
 
+function add_transaction!(ledger::Array{LedgerEntry,1}, transaction::Transaction)
 
-function add_transaction!(ledger::Array{LedgerEntry,1}, mods::Transaction ...)
-    push!(ledger, mods...)
+    if has_account(ledger, transaction.account)
+        push!(ledger, transaction)
+    else
+        println("The transaction, $(transaction), was not added to the ledger.")
+    end
+
+end
+
+function add_account!(ledger::Array{LedgerEntry,1}, account::String; date = today())
+
+    if has_account(ledger, account)
+        println("The account, $(account), was not added to the ledger, because it was already added.")
+    else
+        push!(ledger, AddAccount(account, date))
+    end
+
+end
+
+function remove_account!(ledger::Array{LedgerEntry,1}, account::String; date = today())
+
+    if has_account(ledger, account)
+        push!(ledger, RemoveAccount(account, date))
+    else
+        println("The account, $(account), was not removed from the ledger, because it was never added or was already removed.")
+    end
+
+end
+
+function has_account(ledger::Array{LedgerEntry,1}, account::String)
+    account_exist = false
+
+    for ledger_entry in ledger
+        if typeof(ledger_entry) == AddAccount && ledger_entry.account == account
+            account_exist = true
+        elseif typeof(ledger_entry) == RemoveAccount && ledger_entry.account == account
+            account_exist = false
+        end
+    end
+
+    return account_exist
 end
 
 # Transactions
@@ -42,6 +90,6 @@ end
 # Import
 # csv
 
-export Transaction, new_ledger, add_transaction!, LedgerEntry
+export new_ledger, add_transaction!, add_account!, remove_account!, LedgerEntry, AddAccount, RemoveAccount, Transaction
 
 end # module
